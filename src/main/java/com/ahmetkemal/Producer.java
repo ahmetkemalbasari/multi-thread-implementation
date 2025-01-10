4eca2fe856f5ceeea38fafebbb7e9a414d9ac493
ahmetkemalbasari <akemalbasari@gmail.com>
2025-01-10 17:54:22 +0300

    Implement Producer class

--- src/main/java/com/ahmetkemal/Producer.java
+++ src/main/java/com/ahmetkemal/Producer.java
@@ -1,4 +1,25 @@
 package com.ahmetkemal;
 
-public class Producer {
+import java.util.Random;
+
+public class Producer extends BoundedBuffer {
+
+    public Producer() {
+        while(true) {
+            Integer item = produceItem();
+
+            waitEmpty();
+            waitMutex();
+
+            addItem(item);
+
+            signalMutex();
+            signalFull();
+        }
+    }
+
+    private Integer produceItem(){
+        Random random = new Random();
+        return random.nextInt();
+    }
 }
