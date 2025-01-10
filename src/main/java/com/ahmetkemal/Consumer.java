2f9fca41d9f0317b186910d1beb81d99a0bf5ac5
ahmetkemalbasari <akemalbasari@gmail.com>
2025-01-10 17:58:34 +0300

    Implement Consumer class

--- src/main/java/com/ahmetkemal/Consumer.java
+++ src/main/java/com/ahmetkemal/Consumer.java
@@ -1,4 +1,18 @@
 package com.ahmetkemal;
 
-public class Consumer {
+public class Consumer extends BoundedBuffer {
+
+    public Consumer() {
+        while (true){
+            waitFull();
+            waitMutex();
+            
+            Integer item = getItem();
+
+            signalMutex();
+            signalEmpty();
+
+            System.out.println(item); //consume item
+        }
+    }
 }
