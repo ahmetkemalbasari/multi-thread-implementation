af557e2b8d6d8c53ba9c68749c69784e18bf07eb
ahmetkemalbasari <akemalbasari@gmail.com>
2025-01-12 20:25:14 +0300

    Refactor BoundedBuffer Class
    Changed methods into static methods. Refactored mutex and semophore variables' types.

--- src/main/java/com/ahmetkemal/BoundedBuffer.java
+++ src/main/java/com/ahmetkemal/BoundedBuffer.java
@@ -1,43 +1,55 @@
 package com.ahmetkemal;
 
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BoundedBuffer {
     private static final int BUFFER_SIZE = 16;
     private static LinkedList<Integer> itemBuffer = new LinkedList<>();
-    private static int full = 0;
-    private static int empty = BUFFER_SIZE;
-    private static boolean mutex = false;
+    private static Semaphore full = new Semaphore(0);
+    private static Semaphore empty = new Semaphore(BUFFER_SIZE);
+    private static ReentrantLock mutex = new ReentrantLock();
 
-    public void waitMutex(){
-        while(mutex){};
-        mutex = true;
+    public static void waitMutex() {
+        mutex.lock();
     }
-    public void signalMutex(){
-        mutex = false;
-    }
-
-    public void waitFull(){
-        while(full == 0);
-        full++;
-    }
-    public void signalFull(){
-        full--;
+    public static void signalMutex(){
+        mutex.unlock();
     }
 
-    public void waitEmpty(){
-        while(empty == 0);
-        empty--;
+    public static void waitFull() {
+        try {
+            full.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public void signalEmpty(){
-        empty++;
+    public static void signalFull(){
+        full.release();
     }
 
-    public void addItem(Integer item){
-        itemBuffer.addLast(item);
+    public static void waitEmpty() {
+        try {
+            empty.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public Integer getItem(){
+    public static void signalEmpty(){
+        empty.release();
+    }
+
+    public static void addItem(Integer item){
+        itemBuffer.add(item);
+    }
+    public static Integer getItem(){
         return itemBuffer.removeFirst();
+    }
+
+    public static int getBufferSize(){
+        return BUFFER_SIZE;
     }
 
 }
